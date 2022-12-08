dc2a0f5e3d70ca90e8c0b732ec05a3abdcf80ec8
Sdalpha <satoxonik@gmail.com>
2022-12-08 15:47:30 +0530

    palindrom-chacker submited

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+    console.log(str)
 
+    const len = str.length;
+    for(var i=0;i<len/2;i++){
+        if(str[i]!==str[len-1-i])
+            return false
+    }
+    return true
 }
 module.exports = palindrome
